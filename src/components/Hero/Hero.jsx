8a952327f6564a759c5fb263117716368cee3cc3
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-01 09:44:25 +0530

    Making Hero & Navbar Responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,15 @@ import Button from '../Button/Button'
 const Hero = () => {
   return (
    <section>
-    <div className=' min-h-screen max-w-[1250px] mx-auto px-10 flex items-center pt-25 '>
+    <div className=' min-h-screen max-w-[1250px] mx-auto px-10 flex flex-col  md:flex-row items-center md:pt-25 pt-35  '>
 
         {/* Hero content  */}
         <div className='flex-1'>
-            <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'> Delivering the finest quality....</span>
-            <h1 className='text-7xl/20 font-bold mt-4'>  
-                Testy Organic <span  className='text-orange-500'> Fruits</span> & <span  className='text-orange-500'> Veggies</span><br/>In Your City
+            <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'>Export Best Quality....</span>
+            <h1 className='md:text-7xl/20 font-bold mt-4 text-5xl/14'>  
+                Testy Organic<span  className='text-orange-500'> Fruits</span> & <span  className='text-orange-500'>Veggies</span><br/>In Your City
             </h1>
-            <p className='text-zinc-600 text-lg max-w-[530px] mt-5 mb-10'> 
+            <p className='text-zinc-600 md:text-lg text-md max-w-[530px] mt-5 mb-10'> 
                 Bred for a high content of beneficial substances. Our products are all fresh and healthy.
             </p>
                 <Button  content ="Shop Now"/>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegHeart } from "react-icons/fa";
 import { RiShoppingBag4Fill } from "react-icons/ri";
 import { IoSearch } from "react-icons/io5";
+import { TbMenu2 } from "react-icons/tb";
+import { IoCloseSharp } from "react-icons/io5";
 
 
 const Navbar = () => {
+
+
+    // Write the menu click event Logic
+    const[showMenu, setShowMenu]=useState(false);
+
+    const  toggleMenu =() =>{
+        setShowMenu(!showMenu);
+    }
     return (
         <header className='bg-wh fixed top-0 right-0 left-0'>
-            <nav className='max-w-[1250px] mx-auto px-10 h-[14vh] flex justify-between items-center   '>
+            <nav className='max-w-[1250px] mx-auto px-10 md:h-[14vh] h-[12vh] flex justify-between items-center   '>
 
                 <a href='# ' className='text-3xl font-bold'>
+                    {/* Logo */}
                     Gr<span className='text-orange-500 '>O</span>cify
                 </a>
-                {/* navbar links */}
-                <ul className='flex items-center gap-x-15'>
+                {/* navbar links/ Desktop menu */}
+                <ul className='md:flex items-center gap-x-15 hidden'>
                     <li>
                         <a href='#' className='font-semibold  tracking-wider text-orange-500'> Home</a>
                     </li>
@@ -32,7 +43,7 @@ const Navbar = () => {
                 <div className='flex items-center gap-x-5'>
 
                     {/* input field */}
-                    <div className='flex  p-1 border-2 border-orange-500 rounded-full '>
+                    <div className=' md:flex  p-1 border-2 border-orange-500 rounded-full  hidden'>
                         <input type='text' name='text' id=' text' placeholder=' Search..' autoComplete=' off' className=' flex-1 h-[5vh]  px-3  focus:outline-none' />
 
                         <button className="bg-gradient-to-br from-orange-400 to-orange-600 
@@ -50,8 +61,40 @@ const Navbar = () => {
                     <a href='# ' className='text-zinc-800 text-2xl'>
                         <RiShoppingBag4Fill />
                     </a>
+
+                    {/* hambargar */}
+                    <a href='#' className='text-zinc-800 text-3xl md:hidden  ' onClick={toggleMenu}> 
+                         {showMenu ? <IoCloseSharp /> :<TbMenu2 />  } 
+                    </a>
+
                 </div>
 
+                {/* Mobile Menu */}
+                <ul className= {`flex flex-col gap-y-10 bg-orange-500/30 backdrop-blur-xl rounded-xl p-10 items-center gap-x-15 md:hidden absolute top-30 -right-full transform translate-x-1/2  transition-all duration-500 ${showMenu ? 'right-1/2' :"" }`}>
+                    <li>
+                        <a href='#' className='font-semibold  tracking-wider text-orange-500'> Home</a>
+                    </li>
+                    <li>
+                        <a href='#' className='font-semibold tracking-wider text-zinc-800  hover:text-orange-500 ' > About </a>
+                    </li>
+                    <li>
+                        <a href='#' className='font-semibold tracking-wider text-zinc-800  hover:text-orange-500 '> Process</a>
+                    </li>
+                    <li>
+                        <a href='#' className='font-semibold tracking-wider text-orange-500  text-zinc-800  hover:text-orange-500'> Contact</a>
+                    </li>
+
+                     <li className=' flex  p-1 border-2 border-orange-500 rounded-full  md:hidden'>
+                        <input type='text' name='text' id=' text' placeholder=' Search..' autoComplete=' off' className=' flex-1 h-[5vh]  px-3  focus:outline-none' />
+
+                        <button className="bg-gradient-to-br from-orange-400 to-orange-600 
+                                            text-white w-10 h-10 flex justify-center items-center 
+                                            rounded-full text-xl shadow-md hover:shadow-lg 
+                                            hover:scale-105 transition duration-300 cursor-pointer">
+                            <IoSearch />
+                        </button>
+                    </li>
+                </ul>
             </nav>
         </header>
     )
