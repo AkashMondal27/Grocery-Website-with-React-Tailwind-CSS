f99e72a489ae3a7116e3f6e6e3f053e4038dd963
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-04 00:41:06 +0530

    create the Products Components

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Products = () => {
+  return (
+    <section>
+        <div className='max-w-[1250px]  mx-auto px-10 p-20'>
+            <Heading highlight ="Our" heading="Products"/>
+
+        </div>
+    </section>
+  )
+}
+
+export default Products
