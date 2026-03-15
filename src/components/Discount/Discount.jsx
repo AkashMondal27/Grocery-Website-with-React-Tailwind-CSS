0e64826e67081454a85199d34ed57581e8a80cf5
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-15 08:29:55 +0530

    making Discount Section Responsive

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -5,11 +5,11 @@ import FreshFruits from '../../assets/pictures/fresh-fruits.png'
 const Discount = () => {
   return (
     <section className='bg-zinc-100  bg-no-repeat bg-right  bg-contain' style={{backgroundImage: `url(${FreshFruits})`}}>
-        <div className=' flex max-w-[1250px] mx-auto px-10 py-10'>
-            <span className='text-9xl text-orange-500 font-bold transform -rotate-90  h-fit self-center '> 20%</span>
+        <div className=' md:bd-transparent  flex md:flex-row flex-col max-w-[1250px] mx-auto px-10 py-10'>
+            <span className='md:text-9xl text-6xl text-5xl text-orange-500 font-bold transform md:-rotate-90  h-fit md:self-center '> 20%</span>
             <div className='max-w-[700px]'>
-                <h3 className=' text-7xl text-zinc-800 font-bold'> First order Discount</h3>
-                <p className=' text-zinc-600 my-6'> Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase.  Fast delivery and quality guaranteed.
+                <h3 className='text-4xl md:text-7xl text-red-800 font-bold '> First order Discount</h3>
+                <p className=' md:text-zinc-600 text-zinc-800 my-6 font-bold'> Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase.  Fast delivery and quality guaranteed.
                 </p>
                 <Button content="Get a Discount "/>
             </div>
