60fe1640f0c151745783325e84883fd74bfcd8a5
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-28 09:21:18 +0530

    make all changes for versal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './components/Home/Home'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Fruits from './components/Fruits/Fruits'
 import Dairy from './components/Dairy/Dairy'
 import SeaFood from './components/SeaFood/SeaFood'
@@ -11,11 +11,10 @@ import Discount from './components/Discount/Discount'
 import Testimonials from './components/Testimonials/Testimonials'
 
 
-import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
 
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     {
       path: "/",
       element: <LayOut />,
